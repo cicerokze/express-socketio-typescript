6fb77cae028dcb6b259dad8c925a59f9068c9aa8
Cicero Caze <cicerocaze@gmail.com>
2025-03-15 05:22:10 +0000

    refactor(client): improve readability of code

--- src/scripts/client.js
+++ src/scripts/client.js
@@ -1,42 +1,39 @@
 const socket = io();
-console.log('cliente.js is loaded');
-
 const form = document.getElementById('form');
 const input = document.getElementById('input');
 const messages = document.getElementById('messages');
 
+/**
+ * Listen on event 'connect' and logs to server
+ * checking when connection is successfully established
+ */
 socket.on('connect', () => {
-    console.log('socket.id: ', socket.id);
-    sendMsg();
-    sendGlobalMsg(
-        socket.id,
-        'Bem vindo!',
-    );
+    renderGlobalMsg(socket.id, ' connected');
 });
 
-socket.on('chat message', (msg) => {
-    console.log('chat message, msg: ', msg.replace(/<b>|<\/b>/g, ''));
+/**
+ * Listen on event 'global-chat' and renders
+ * all messages in the screen of any client to all clients
+ */
+socket.on('global-chat', (msg) => {
+    console.log('global-chat, msg: ', msg.replace(/<b>|<\/b>/g, ''));
     const li = document.createElement('li');
     li.innerHTML = msg;
     messages.appendChild(li);
     window.scrollTo(0, document.body.scrollHeight);
 });
 
-function sendMsg() {
+/**
+ * Send input message on the event 'global-chat' for all clients
+ * @param {string} senderId 
+ * @param {string} msg 
+ */
+function renderGlobalMsg(senderId, msg) {
     form.addEventListener('submit', (e) => {
         e.preventDefault();
         if (input.value) {
-            console.log('input.value: ', input.value);
-            socket.emit('chat message', `<b>${socket.id}</b>: ` + input.value);
+            socket.emit('global-chat', `<b>${socket.id}</b>: ${input.value}`);
             input.value = '';
         }
-    });
-}
-
-//Client sent a message 'world' to the server
-function sendGlobalMsg(senderId, msg) {
-    socket.emit('hello', `${senderId}: ${msg}`);
-    socket.on('hello', (arg) => {
-        console.log('hello, arg: ', arg);
     });
 }
